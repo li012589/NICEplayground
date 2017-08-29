fd67e2f06dae9b037b4c5307fd43300fc6f9331b
Li SH <li012589li@outlook.com>
2017-08-29 15:42:35 +0800

    add some function

--- hmc/hmc.py
+++ hmc/hmc.py
@@ -1,8 +1,55 @@
 import tensorflow as tf
+from utils/expLogger import expLogger
 import numpy as np
 
+def kineticEnergy(v):
+    '''
+    Return the value of the kineticEnergy
+    :param v: velocity variable
+    :return: kineticEnergy
+    '''
+    return 0.5*tf.reduce_sum(tf.multiply(v,v),axis=1)
 
+def hamilitonian(p,v,f):
+    '''
+    Return the value of the Hamiltonian
+    :param p: position variable
+    :param v: velocity variable
+    :param f: energy function
+    :return: hamiltonian
+    '''
+    return f(p)+kineticEnergy(v)
 
+def metropolisHastingsAccept(energyPre,eneryNext,expLogger,ifuseLogger = False):
+    """
+    Run Metropolis-Hastings algorithm for 1 step
+    :param energyPrev: the original energy from before
+    :param energyNext: the energy after evolution
+    :return: Tensor of boolean values, indicating accept or reject
+    """
+    energyDiff = energyPre - eneryNext
+    if ifuseLogger:
+        return expLogger.cal(energyDiff) # TODO: The energyDiff is a tf tensor object, need some workaround
+    return (tf.exp(energyDiff)) - tf.random_uniform(tf.shape(energyPre)) >= 0.0
+
+def simulateDynameics(initialPos,initialV,stepSize,steps,energyFn):
+    """
+    Run Hamilitonian evolution for some step
+    :param intialPos: the states of field before evolution
+    :param intiialV: the velocity before evolution
+    :param stepSize: the size of a single step
+    :param steps: total steps in the evolution
+    :param energyFn: the energy fucntion used in the evolution
+    :return newPos: the states of the field after evolution
+    :return newV: the velocity after evolution
+    """
+    pass
+
+def leapfrog(pos,vel,step,i):
+    '''
+    The leapfrog integrator
+    :param 
+    '''
 
 def main():
     pass

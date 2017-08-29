6ea825fe77bfd0534600f71fb2ce293f33776521
Li SH <li012589li@outlook.com>
2017-08-29 20:57:27 +0800

    doubleGaussian finished and tested

---
+++ model/doubleGaussian.py
@@ -0,0 +1,36 @@
+import numpy as np
+import tensorflow as tf
+
+class doubleGaussian:
+    '''
+    A class represent doubleGaussian, when called return the energy function value.
+    '''
+    def __init__(self,name="doubleGaussian"):
+        self.name = name
+        self.z = tf.placeholder(tf.float32,[None,2])
+
+    def __call__(self,z):
+        with tf.variable_scope(self.name):
+            z1 = tf.reshape(tf.slice(z,[0,0],[-1,1]),[-1])
+            z2 = tf.reshape(tf.slice(z,[0,1],[-1,1]),[-1])
+            v = tf.multiply(z1,z1)/2+tf.multiply(z2,z2)/2+tf.multiply(z1,z2)
+            return v
+    def mean(self):
+        pass
+    def std(self):
+        pass
+
+def main():
+    '''
+    Test script for doubleGaussian
+    '''
+    sess = tf.InteractiveSession()
+    a = doubleGaussian("test")
+    z = np.random.normal(0,1,[2,2])
+    print(z)
+    print(sess.run(a(z)))
+    print(z[0][0])
+    print(z[0][1])
+    print(z[0][0]**2/2+z[0][1]**2/2+z[0][0]*z[0][1])
+if __name__ =="__main__":
+    main()

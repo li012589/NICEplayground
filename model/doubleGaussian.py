53068ce2358536618581443056370f3df2ecacb7
Li SH <li012589li@outlook.com>
2017-08-30 15:28:44 +0800

    add mean std and measure to doubleGuassian

--- model/doubleGaussian.py
+++ model/doubleGaussian.py
@@ -15,10 +15,12 @@ class doubleGaussian:
             z2 = tf.reshape(tf.slice(z,[0,1],[-1,1]),[-1])
             v = tf.multiply(z1,z1)/2+tf.multiply(z2,z2)/2+tf.multiply(z1,z2)
             return v
-    def mean(self):
-        pass
-    def std(self):
-        pass
+    def mean(self,z):
+        return np.mean(z,axis=1)
+    def std(self,z):
+        return np.std(z,axis=1)
+    def measure(self,z,n):
+        return np.mean(np.power(z,n),axis=1)
 
 def main():
     '''
@@ -32,5 +34,6 @@ def main():
     print(z[0][0])
     print(z[0][1])
     print(z[0][0]**2/2+z[0][1]**2/2+z[0][0]*z[0][1])
+    print(a.mean(z))
 if __name__ =="__main__":
     main()

ef92319b55e31d06e71d0c269a6bfd2b713044ef
Li SH <li012589li@outlook.com>
2017-08-31 20:07:09 +0800

    add test script for metropolis

--- measure.py
+++ measure.py
@@ -7,6 +7,7 @@ from model.ring2d import Ring2d
 from model.doubleGaussian import doubleGaussian
 from model.phi4 import phi4
 from hmc.hmc import HMCSampler
+from Metropolois.Metropolis import MHSampler
 
 def prior(bs):
     return np.random.normal(0,1,[bs,9])
@@ -15,10 +16,18 @@ if __name__ == "__main__":
     #energyFn = doubleGaussian()
     energyFn = phi4(9,3,2,1,1)
     hmc = HMCSampler(energyFn,prior)
+    mh = MHSampler(energyFn,prior)
+    print("HMC")
     z_ = hmc.sample(80,8)
     z_ = z_[:,30:]
     z_ = np.reshape(z_,[-1,2])
     x_,y_ = z_[:,0],z_[:,1]
     print(np.mean(x_))
     print(np.std(x_))
-    #print(z)
+    print("MH")
+    z_ = mh.sample(80,8)
+    z_ = z_[:,30:]
+    z_ = np.reshape(z_,[-1,2])
+    x_,y_ = z_[:,0],z_[:,1]
+    print(np.mean(x_))
+    print(np.std(x_))

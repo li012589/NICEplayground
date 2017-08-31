775d3f5abf532e70431a068691f11f81cf25ef59
Li SH <li012589li@outlook.com>
2017-08-31 18:11:53 +0800

    debug

--- Metropolois/Metropolis.py
+++ Metropolois/Metropolis.py
@@ -16,21 +16,35 @@ class MHSampler():
         self.energyFn = energyFn
         self.prior = prior
         self.z = self.energyFn.z
+        expLog = expLogger({})
         def fn(z,x):
             z_ = z+tf.random_normal(tf.shape(self.z),0.0,std)
-            accept = metropolisHastingsAccept(energyFn(z),energyFn(z_))
+            accept = metropolisHastingsAccept(energyFn(z),energyFn(z_),expLog)
             return tf.where(accept,z_,z)
         self.steps = tf.placeholder(tf.int32,[])
         elems = tf.zeros([self.steps])
         self.z_ = tf.scan(fn,elems,self.z,back_prop=False)
         self.sess = tf.InteractiveSession()
-        self.sess.run(tf.global_variables_initializer)
+        self.sess.run(tf.global_variables_initializer())
     def sample(self,steps,batchSize):
         z = self.sess.run(self.z_,feed_dict={self.steps:steps,self.z:self.prior(batchSize)})
         z = np.transpose(z,[1,0,2])
         return z
 def main():
-    pass
+    from model.ring2d import Ring2d
+    from model.doubleGaussian import doubleGaussian
+    from model.phi4 import phi4
+
+    def prior(bs):
+        return np.random.normal(0,1,[bs,2])
+    energyFn = Ring2d()
+    MH = MHSampler(energyFn,prior)
+    z_ = MH.sample(8000,800)
+    z_ = z_[:,3000:]
+    z_ = np.reshape(z_,[-1,2])
+    x_,y_ = z_[:,0],z_[:,1]
+    print(np.mean(x_))
+    print(np.std(x_))
 
 if __name__ == "__main__":
     main()

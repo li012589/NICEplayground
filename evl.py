e29f632079ed26dc829becddaf4d2373393c1232
Li SH <li012589li@outlook.com>
2017-09-05 00:40:09 +0800

    add evaluation  script

---
+++ evl.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import numpy as np
+from model.ring2d import Ring2d
+from model.TGaussian import TGaussian
+#from model.doubleGaussian import doubleGaussian
+#from model.Ising import Ising
+#from model.phi4 import phi4
+from hmc.hmc import HMCSampler
+from Metropolois.Metropolis import MHSampler
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def prior(bs):
+    return np.random.normal(0,1,[bs,3])
+
+if __name__ == "__main__":
+    energyFn = TGaussian()
+    hmc = HMCSampler(energyFn,prior)
+    mh = MHSampler(energyFn,prior)
+    print("HMC")
+    z_ = hmc.sample(8000,800)
+    z_ = z_[:,3000:]
+    z_ = np.reshape(z_,[-1,3])
+    z1_,z2_,z3_ = z_[:,0],z_[:,1],z_[:,2]
+    print(np.mean(z1_))
+    print(np.std(z1_))
+    print("MH")
+    z_ = mh.sample(8000,800)
+    z_ = z_[:,3000:]
+    z_ = np.reshape(z_,[-1,3])
+    z1_,z2_,z3_ = z_[:,0],z_[:,1],z_[:,2]
+    print(np.mean(z1_))
+    print(np.std(z1_))

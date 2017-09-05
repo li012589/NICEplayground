e751e9d4064d0017edf522c3754819eb123fbde6
Li SH <li012589li@outlook.com>
2017-09-05 22:58:52 +0800

    add parameterInit

---
+++ utils/parameterInit.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev = 0.01)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.01, shape = shape)
+    return tf.Variable(initial)

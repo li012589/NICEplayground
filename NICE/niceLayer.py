bffd65d08a36b45ed989198edae4f4e5a0194520
Li SH <li012589li@outlook.com>
2017-09-05 23:00:46 +0800

    remove parameter initialize lines

--- NICE/niceLayer.py
+++ NICE/niceLayer.py
@@ -1,14 +1,6 @@
 import tensorflow as tf
 import numpy as np
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
-    return tf.Variable(initial)
-
 class NiceLayer:
     def __init__(self,dims,network,name="niceLayer",swap=False):
         '''

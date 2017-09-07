2ec1e3813c0ec0f157a5cfc6c38d24ffdde120f3
Li SH <li012589li@outlook.com>
2017-09-07 18:28:29 +0800

    operator forward works

--- NICEMC/NICEMC.py
+++ NICEMC/NICEMC.py
@@ -10,21 +10,25 @@ from utils.MetropolisHastingsAccept import metropolisHastingsAccept
 
 class NiceNetworkOperator:
     def __init__(self,network,energyFn):
-        self.network = lambda inputs,direction: tf.cond(direction,network.forward(inputs),network.backward(inputs))
+        self.network = lambda inputs,t: tf.cond(t>0.5,lambda: network.forward(inputs),lambda: network.backward(inputs))
         self.energyFn = energyFn
     def __call__(self,inputs,steps,vDim,ifMH):
+        def networkInterface(inputs,direction):
+            return tf.cond(direction,self.network.forward(inputs),self.network.backward(inputs))
         if ifMH:
-            def fn(z):
-                v = tf.random_normal([z.get_shape[0],vDim])
+            def fn(zv,step):
+                z,v = zv
+                v = tf.random_normal([z.get_shape().as_list()[0],vDim])
                 z_,v_ = self.network([z,v],(tf.random_uniform([]) < 0.5))
                 accept = metropolisHastingsAccept(self.energyFn(z,v))
                 z_ = tf.where(accept,z_,z)
                 return z_,v_
         else:
-            def fn(z):
-                v = tf.random_normal([z.get_shape[0],vDim])
-                z_,v_ = self.network([z,v],True)
-                return z_,v_
+            def fn(zv,step):
+               z, v = zv
+               v = tf.random_normal(shape=tf.stack([tf.shape(z)[0],vDim]))
+               z_, v_ = self.network([z,v],tf.constant(1,dtype=tf.float32))
+               return z_, v_
         elems = tf.zeros([steps])
         return tf.scan(fn,elems,inputs,back_prop=False)
 
@@ -42,4 +46,46 @@ class NICEMCSampler:
         pass
 
 if __name__ == "__main__":
-    pass
+    '''
+    Test script
+    '''
+    from utils.parameterInit import weightVariable, biasVariable
+    from model.TGaussian import TGaussian
+
+    mod = TGaussian("test")
+
+    def Fixlayer(inputs, num_outputs,name,reuseMark):
+        #print(name)
+        w = np.array([[1,0],[0,1]])
+        b = np.array([[1],[1]])
+        w_ = tf.convert_to_tensor(w,dtype=tf.float32)
+        b_ = tf.convert_to_tensor(b,dtype=tf.float32)
+        ret = tf.matmul(inputs,w_)+b_
+        return ret
+
+    def randomLayer(inputs,num_outputs,name,reuseMark = None):
+        with tf.variable_scope(name,reuse=reuseMark):
+            wFC = weightVariable("RandomLayerFCw",[inputs.get_shape()[-1],num_outputs])
+            bFC = biasVariable("RandomLayerFC",[num_outputs])
+            fc = tf.matmul(inputs,wFC)+bFC
+            fc = tf.nn.relu(fc)
+            return fc
+
+    net = NiceNetwork()
+    args1 = [([10,5,3],'v1',False),([10,10],'x1',True),([10,10],'v2',False)]
+    #args = [([2],'x1',True),([2],'v1',False),([2],'x2',True)]
+    for dims, name ,swap in args1:
+        net.append(NiceLayer(dims,randomLayer,name,swap))
+    Operator = NiceNetworkOperator(net,mod)
+    z = np.array([[1,2,3],[2,3,4]])
+    z_ = tf.convert_to_tensor(z,dtype=tf.float32)
+    vDim = 3
+    v_ = tf.random_normal([z_.get_shape().as_list()[0],vDim])
+    inputs = (z_,v_)
+    #v = tf.random_normal([z_.get_shape().as_list()[0],vDim])
+    #inputs = [z_,v]
+    #net.forward(inputs)
+    ret = Operator(inputs,2,3,False)
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(ret))

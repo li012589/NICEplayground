5ac500ef3c1c317672ad358649d335da593d54c4
Li SH <li012589li@outlook.com>
2017-09-15 00:37:20 +0800

    change default init method

--- utils/parameterInit.py
+++ utils/parameterInit.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
-def weightVariable(name,shape,init = None):
+def weightVariable(name,shape,init = tf.truncated_normal_initializer(stddev=0.02)):
     initial = tf.get_variable(name,shape,dtype=tf.float32, initializer=init)
     return initial
 
-def biasVariable(name,shape,init = None):
+def biasVariable(name,shape,init = tf.truncated_normal_initializer(stddev=0.02)):
     initial = tf.get_variable(name,shape,initializer=init)
     return initial

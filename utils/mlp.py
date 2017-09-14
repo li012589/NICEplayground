2c09a76cc4658688b4e8a20e9429c1cd05be8efa
Li SH <li012589li@outlook.com>
2017-09-14 23:35:45 +0800

    change last layer to tf.identity

--- utils/mlp.py
+++ utils/mlp.py
@@ -18,16 +18,21 @@ class mlp:
 
     def __call__(self,fc):
         for i,w in enumerate(self.W):
-            fc = self.active(tf.matmul(fc,w)+self.B[i])
+            if i != len(self.W)-1:
+                fc = self.active(tf.matmul(fc,w))+self.B[i]
+            else:
+                fc = (tf.matmul(fc,w)+self.B[i])
         return fc
 
 if __name__ == "__main__":
     import numpy as np
-    net = mlp([[2,2],[2,3],[3,50],[50,2]],tf.nn.relu,"test")
+    net = mlp([[2,40],[40,2]],tf.nn.relu,"test")
     print(net.W)
-    i = np.array([[1,2],[2,1],[2,3],[4,5]])
+    i = np.array([[-2,-1],[-1,-3],[-2,-1],[-5,-1]])
     i_ = tf.convert_to_tensor(i,dtype=tf.float32)
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
     ret = sess.run(net(i_))
     print(ret)
+    print(sess.run(net.W))
+    print(sess.run(net.B))

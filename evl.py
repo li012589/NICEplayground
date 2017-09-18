3fdd6cd09498524adc0ec51d78ddcc21da7c3f18
Li SH <li012589li@outlook.com>
2017-09-18 17:48:37 +0800

    add acceptanceRatio

--- evl.py
+++ evl.py
@@ -15,8 +15,10 @@ from utils.acceptRate import acceptance_rate
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+zDim = 2
+
 def prior(bs):
-    return np.random.normal(0,1,[bs,2])
+    return np.random.normal(0,1,[bs,zDim])
 def priorPhi4(bs):
     return np.random.normal(0,1,[bs,9])
 
@@ -27,25 +29,29 @@ if __name__ == "__main__":
     mh = MHSampler(energyFn,prior)
     print("HMC")
     z = hmc.sample(800,80)
-    z_ = z[:,300:]
-    z_ = np.reshape(z_,[-1,2])
+    z_o = z[:,300:]
+    z_ = np.reshape(z_o,[-1,2])
     z1_,z2_ = z_[:,0],z_[:,1]
     print(np.mean(z1_))
     print(np.std(z1_))
     print("autoCorrelationTime:")
     print(autoCorrelationTime(z_,7))
     #print(autoCorrelationTime(z1_,7))
+    print("acceptance:")
+    print(acceptance_rate(np.transpose(z_o,[1,0,2])))
     #print(autoCorrelationTime(z2_,7))
     #print(autoCorrelationTime(z3_,7))
     print("MH")
     z_ = mh.sample(800,80)
-    z_ = z_[:,300:]
-    z_ = np.reshape(z_,[-1,2])
+    z_o = z_[:,300:]
+    z_ = np.reshape(z_o,[-1,2])
     z1_,z2_= z_[:,0],z_[:,1]
     print(np.mean(z1_))
     print(np.std(z1_))
     print("autoCorrelationTime:")
     print(autoCorrelationTime(z_,7))
     #print(autoCorrelationTime(z1_,7))
+    print("acceptance:")
+    print(acceptance_rate(np.transpose(z_o,[1,0,2])))
     #print(autoCorrelationTime(z2_,7))
     #print(autoCorrelationTime(z3_,7))

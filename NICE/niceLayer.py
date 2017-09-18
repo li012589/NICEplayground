2bf3e6b76d208e1b9cf37a978a0575a9cd852403
Li SH <li012589li@outlook.com>
2017-09-18 15:09:04 +0800

    add test case for Jacbian equal 1

--- NICE/niceLayer.py
+++ NICE/niceLayer.py
@@ -4,7 +4,13 @@ if __name__ == "__main__":
     sys.path.append(os.getcwd())
 
 import tensorflow as tf
+import tensorflow.contrib.layers as tcl
 import numpy as np
+
+def dense(inputs, num_outputs, activation_fn=tf.identity, normalizer_fn=None, normalizer_params=None):
+    return tcl.fully_connected(inputs, num_outputs, activation_fn=activation_fn,
+                               normalizer_fn=normalizer_fn, normalizer_params=normalizer_params)
+
 
 class NiceLayer:
     def __init__(self,dims,network,active,name="niceLayer",swap=False):
@@ -82,7 +88,7 @@ if __name__ == "__main__":
     #args = [([2],'x1',True),([2],'v1',False),([2],'x2',True)]
     for dims, name ,swap in args1:
         net.append(NiceLayer(dims,mlp,tf.nn.relu,name,swap))
-    z = np.array([[2,3],[1,2]],dtype=np.float32)
+    z = np.array([[2,3]],dtype=np.float32)
     v = z+1
     inputs=[z,v]
     print(inputs)
@@ -116,8 +122,69 @@ if __name__ == "__main__":
     assert (np.allclose(inputs,ret2_r)), "Input doesn't match backward"
     assert (np.allclose(forward,ret2r)), "First forward doesn't math second forward, the parameter in inner layer may not be same'"
 
-    print("compute gradients")
+    #print("compute gradients")
+    #print(sess.run(ret[0][0]))
+    #print(sess.run(inputs_[0][0]))
+    #g = sess.run(tf.gradients(ret,inputs_[0]))
+    #g2 = sess.run(tf.gradients(ret,inputs_[1]))
+    #print(g)
+    #print(g2)
+    #print("det:")
+    grads = tf.stack([tf.gradients(yi, inputs_)[0] for yi in tf.unstack(ret[0], axis=1)],
+                     axis=2)
+    print(sess.run([yi for yi in tf.unstack(ret[0],axis = 1)]))
+    print(sess.run(grads))
+
+    net2 = NiceNetwork()
+    args1 = [([[1,10],[10,5],[5,1]],'v1x',False),([[1,10],[10,1]],'x1x',True),([[1,4],[4,1]],'v2x',False)]
+    #args = [([2],'x1',True),([2],'v1',False),([2],'x2',True)]
+    for dims, name ,swap in args1:
+        net2.append(NiceLayer(dims,mlp,tf.nn.relu,name,swap))
+    z = np.array([[-1]],dtype = np.float32)
+    v = z+1
+    inputs=[z,v]
+    print(inputs)
+    z_ = tf.convert_to_tensor(z,dtype=tf.float32)
+    v_ = tf.convert_to_tensor(v,dtype=tf.float32)
+    inputs_ = [z_,v_]
+    ret = net2.forward(inputs_)
+    ret2 = net2.forward(inputs_)
+    ret2_ = net2.backward(ret2)
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    forward = (sess.run(ret))
+    print("forwarding")
+    print(forward)
+    zp_ = tf.convert_to_tensor(forward[0],dtype=tf.float32)
+    vp_ = tf.convert_to_tensor(forward[1],dtype=tf.float32)
+    forward_ = [zp_,vp_]
+    retp = net2.backward(forward_)
+    backward = sess.run(retp)
+    print("backwarding")
+    print(backward)
+    assert (np.allclose(inputs,backward)), "Input doesn't match backward"
+    print("Input matched backward")
+    print("2nd test:")
+    print("forward")
+    ret2r = sess.run(ret2)
+    print(ret2r)
+    print("backward")
+    ret2_r = sess.run(ret2_)
+    print(ret2_r)
+    assert (np.allclose(inputs,ret2_r)), "Input doesn't match backward"
+    assert (np.allclose(forward,ret2r)), "First forward doesn't math second forward, the parameter in inner layer may not be same'"
+
+    print("Computing gradients")
     g = sess.run(tf.gradients(ret,inputs_))
-    print(g)
-    print("det:")
-    print(np.linalg.det(g))
+    g0 = sess.run(tf.gradients(ret[0],inputs_))
+    g1 = sess.run(tf.gradients(ret[1],inputs_))
+    g00 = sess.run(tf.gradients(ret[0],inputs_[0]))
+    g01 = sess.run(tf.gradients(ret[0],inputs_[1]))
+    g10 = sess.run(tf.gradients(ret[1],inputs_[0]))
+    g11 = sess.run(tf.gradients(ret[1],inputs_[1]))
+    #print(g)
+    print(g0)
+    print(g1)
+    det = g00[0]*g11[0]-g01[0]*g10[0]
+    print(det)
+    assert (np.isclose(1,det)) , "Determinant of Jacobian is not equal 1"

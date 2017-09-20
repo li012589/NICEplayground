4f4418e0872753de5a0a6336e28bc9095c3f6401
Li SH <li012589li@outlook.com>
2017-09-20 14:40:05 +0800

    add separate file for tran and smaple

--- testscript/NICEtrain.py
+++ testscript/NICEtrain.py
@@ -7,12 +7,11 @@ import tensorflow as tf
 import numpy as np
 from NICE.niceLayer import NiceLayer,NiceNetwork
 from NICEMC.NICEMC import NICEMCSampler
-from utils.autoCorrelation import autoCorrelationTime
-from utils.acceptRate import acceptance_rate
+#from utils.autoCorrelation import autoCorrelationTime
+#from utils.acceptRate import acceptance_rate
 
 def leaky_relu(x, alpha=0.2):
     return tf.maximum(tf.minimum(0.0, alpha * x), x)
-
 
 #from utils.parameterInit import weightVariable, biasVariable
 from model.TGaussian import TGaussian
@@ -25,7 +24,7 @@ def prior(batchSize):
     return np.random.normal(0,1,[batchSize,s])
 
 #mod = TGaussian("test")
-mod = Ring2d()
+mod = Ring2d("test")
 #mod = phi4(9,3,2,1,1)
 net = NiceNetwork()
 args1 = [([[s,400],[400,s]],'generator/v1',tf.nn.relu,False),([[s,400],[400,s]],'generator/x1',tf.nn.relu,True),([[s,400],[400,s]],'generator/v2',tf.nn.relu,False)]
@@ -36,24 +35,4 @@ b = 8
 m = 2
 dnet = mlp([[2*s,400],[400,400],[400,400],[400,1]],leaky_relu,"discriminator")
 sampler = NICEMCSampler(mod,prior,net,dnet,b,m,'./savedNetwork','./tfSummary')
-
-z,v = sampler.sample(800,100,True,True)
-#print(z)
-print(z.shape)
-z = z[300:,:]
-#print(z)
-print(z.shape)
-autocorrelation = autoCorrelationTime(z,7)
-acceptRate = acceptance_rate(z)
-#print(acceptRate)
-print("autoCorrelation: ",autocorrelation,"acceptRate: ",acceptRate)
-z_ = np.reshape(z,[-1,2])
-z0,z1 = z_[:,0],z_[:,1]
-print(np.mean(z0))
-print(np.std(z0))
-
-print(np.mean(z1))
-print(np.std(z1))
-
-#sampler.train(2,10,10,5,5,2,2,10,5,2,2,2,True,True)
-#sampler.train(500,100000,5000,32,1000,100,32,5000,1000,5,32,1000,True,False)
+sampler.train(500,100000,5000,32,1000,100,32,5000,1000,5,32,1000,True,False)

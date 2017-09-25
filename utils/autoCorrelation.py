0cb06f1f1bb9fd65db25f5bed847171fdc43b23c
Li SH <li012589li@outlook.com>
2017-09-25 15:20:50 +0800

    debug, now autoCorrelation can handle batch

--- utils/autoCorrelation.py
+++ utils/autoCorrelation.py
@@ -1,19 +1,30 @@
 import numpy as np
 
 def binning_analysis(samples,bins):
+    '''
+    Calculate errors using binning method
+    :param samples: [TimeStep,BatchSize,zSize], the data to be evaluated
+    :param bins: int, minimum bin size = 2**bins
+    :return errors: [binsNum,BatchSize], std errors
+    :return mean_errors: [binsNum], = np.mean(errors,1)
+    '''
     #Perform a binning analysis over samples and return an array of the error estimate at each binning level.
     minbins = 2**bins # minimum number of bins (128 still seems to be a reasonable sample size in most cases)
     maxlevel = int(np.log2(len(samples)/minbins))
     maxsamples = int(minbins * 2**(maxlevel))
-    bins = np.array(samples[-maxsamples:]) # clip to power of 2 for simplicity
-    errors = np.zeros(maxlevel+1)
+    bins = np.array(samples[-maxsamples:,:]) # clip to power of 2 for simplicity
+    #errors = np.zeros(maxlevel+1)
+    errors = []
     for k in range(maxlevel+1):
-        errors[k] = np.std(bins)/np.sqrt(len(bins)-1.)
+        errors.append(np.std(bins,0)/np.sqrt(len(bins)-1.))
         bins = np.array([(bins[2*i]+bins[2*i+1])/2. for i in range(len(bins)//2)])
-    return errors
+    errors = np.array(errors)
+    mean_errors = np.mean(errors,1)
+    return errors,mean_errors
 
 def auto_time(errors):
   return  .5*(errors[-1]**2/errors[0]**2 - 1.)
 
 def autoCorrelationTime(samples,bins=7):
-    return auto_time(binning_analysis(samples,bins))
+    _,errors = binning_analysis(samples,bins)
+    return auto_time(errors)

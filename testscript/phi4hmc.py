a99e82ba458d47d5c281a20f9058d938078f2900
Li SH <li012589li@outlook.com>
2017-09-27 23:54:56 +0800

    add multi lambda to evulate

--- testscript/phi4hmc.py
+++ testscript/phi4hmc.py
@@ -23,33 +23,32 @@ n = 27
 dim = 3
 l = 3
 kappa = 1.145
-lamb = 0.14
-Lamb = [i/100 for i in range(14,22)]
-print(Lamb)
-
-energyFn = phi4(n,l,dim,kappa,lamb)
-
-'''Define sampler'''
-hmc = HMCSampler(energyFn,prior)
-mh = MHSampler(energyFn,prior)
-
+lamb = 0.15
+Lamb = [i/100 for i in range(15,22)]
+#print(Lamb)
 '''Start sampling'''
 TimeStep = 800
 BatchSize = 100
 BurnIn = 300
 bins = 2
 
-print("HMC")
-z = hmc.sample(TimeStep,BatchSize)
-z_o = z[BurnIn:,:]
-z_ = np.reshape(z_o,[-1,zSize])
-print("mean: ",np.mean(z_))
-print("std: ",np.std(z_))
-zt = np.mean(z_o,2)
-autoCorrelation =  autoCorrelationTime(zt,bins)
-acceptRate = acceptance_rate(z_o)
-print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
-
+for lamb in Lamb:
+    energyFn = phi4(n,l,dim,kappa,lamb)
+    '''Define sampler'''
+    hmc = HMCSampler(energyFn,prior)
+    mh = MHSampler(energyFn,prior)
+    print("HMC")
+    z = hmc.sample(TimeStep,BatchSize)
+    z_o = z[BurnIn:,:]
+    #print(z_o.shape)
+    z_ = np.reshape(z_o,[-1,zSize])
+    m_abs = np.absolute(z_o)
+    m_abs = np.mean(m_abs,2)
+    print(np.mean(m_abs))
+    autoCorrelation =  autoCorrelationTime(m_abs,bins)
+    acceptRate = acceptance_rate(z_o)
+    print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
+'''
 print("MH")
 z_ = mh.sample(TimeStep,BatchSize)
 z_o = z_[BurnIn:,:]
@@ -60,3 +59,4 @@ zt = np.mean(z_o,2)
 autoCorrelation =  autoCorrelationTime(zt,bins)
 acceptRate = acceptance_rate(z_o)
 print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
+'''

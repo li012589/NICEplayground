42c50e43877561f7f6e5cdee3b9f4d7e0cb8950b
Li SH <li012589li@outlook.com>
2017-09-28 23:11:24 +0800

    debug

--- testscript/phi4hmc.py
+++ testscript/phi4hmc.py
@@ -23,8 +23,8 @@ n = 27
 dim = 3
 l = 3
 kappa = 1.145
-lamb = 0.15
-Lamb = [i/100 for i in range(15,22)]
+#lamb = 0.15
+Lamb = [0.15]#[i/100 for i in range(15,22)]
 #print(Lamb)
 '''Start sampling'''
 TimeStep = 800
@@ -32,31 +32,23 @@ BatchSize = 100
 BurnIn = 300
 bins = 2
 
+res = []
 for lamb in Lamb:
+    print(lamb)
     energyFn = phi4(n,l,dim,kappa,lamb)
     '''Define sampler'''
     hmc = HMCSampler(energyFn,prior)
-    mh = MHSampler(energyFn,prior)
-    print("HMC")
     z = hmc.sample(TimeStep,BatchSize)
     z_o = z[BurnIn:,:]
-    #print(z_o.shape)
-    z_ = np.reshape(z_o,[-1,zSize])
+    print(z_o[0,-10:,:])
     m_abs = np.absolute(z_o)
-    m_abs = np.mean(m_abs,2)
-    print(np.mean(m_abs))
+    m_abs = np.mean(m_abs,2)/zSize
+    print(m_abs.shape)
+    m_abs_p = np.mean(m_abs)
+    print(m_abs_p)
+    res.append(m_abs_p)
     autoCorrelation =  autoCorrelationTime(m_abs,bins)
     acceptRate = acceptance_rate(z_o)
     print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
-'''
-print("MH")
-z_ = mh.sample(TimeStep,BatchSize)
-z_o = z_[BurnIn:,:]
-z_ = np.reshape(z_o,[-1,zSize])
-print("mean: ",np.mean(z_))
-print("std: ",np.std(z_))
-zt = np.mean(z_o,2)
-autoCorrelation =  autoCorrelationTime(zt,bins)
-acceptRate = acceptance_rate(z_o)
-print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
-'''
+
+print(res)

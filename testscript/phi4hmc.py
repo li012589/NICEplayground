e9997a2e6ed2e1a3ed2fa3dec90d8d86af2428f2
Li SH <li012589li@outlook.com>
2017-10-12 17:31:46 +0800

    update hmc method

--- testscript/phi4hmc.py
+++ testscript/phi4hmc.py
@@ -10,19 +10,17 @@ from utils.acceptRate import acceptance_rate
 
 from model.phi4 import phi4
 
-'''Setting model's size'''
-zSize = 216
+
+'''Define the model to evaluate'''
+dim = 3
+l = 6
+n = l**dim
+Lamb = [1.145]
+Kappa = [i/100 for i in range(15,22+1)]
 
 '''define sampler to initialize'''
 def prior(batchSize):
-    return np.random.normal(0,1,[batchSize,zSize])
-
-'''Define the model to evaluate'''
-n = 216
-dim = 3
-l = 6
-lamb = 1.145
-KAPPA = [i/100 for i in range(15,22+1)]
+    return np.random.normal(0,1,[batchSize,n])
 #print(Lamb)
 '''Start sampling'''
 TimeStep = 800
@@ -32,24 +30,26 @@ bins = 2
 
 res = []
 errors = []
-for kappa in KAPPA:
-    energyFn = phi4(n,l,dim,kappa,lamb)
-    '''Define sampler'''
-    hmc = HMCSampler(energyFn,prior)
-    z = hmc.sample(TimeStep,BatchSize)
-    z_o = z[BurnIn:,:]
-    #print(z_o[0,-10:,:])
-    m_abs = np.mean(z_o,2)
-    m_abs = np.absolute(m_abs)
-    #print(m_abs.shape)
-    m_abs_p = np.mean(m_abs)
-    res.append(m_abs_p)
-    autoCorrelation,error =  autoCorrelationTimewithErr(m_abs,bins)
-    acceptRate = acceptance_rate(z_o)
-    print("kappa:",kappa)
-    print("measure: <|m|/V>",m_abs_p,"with error:",error)
-    errors.append(error)
-    print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
+energyFn = phi4(n,l,dim,0.15,1.145)
+for lamb in Lamb:
+    for kappa in Kappa:
+        '''Define sampler'''
+        energyFn.reload(n,l,dim,kappa,lamb)
+        hmc = HMCSampler(energyFn,prior)
+        z = hmc.sample(TimeStep,BatchSize)
+        z_o = z[BurnIn:,:]
+        #print(z_o[0,-10:,:])
+        m_abs = np.mean(z_o,2)
+        m_abs = np.absolute(m_abs)
+        #print(m_abs.shape)
+        m_abs_p = np.mean(m_abs)
+        res.append(m_abs_p)
+        autoCorrelation,error =  autoCorrelationTimewithErr(m_abs,bins)
+        acceptRate = acceptance_rate(z_o)
+        print("kappa:",kappa)
+        print("measure: <|m|/V>",m_abs_p,"with error:",error)
+        errors.append(error)
+        print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
 
 print("measure: <|m|/V>")
 print(res)
